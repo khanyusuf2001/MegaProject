7949b68b94425ce012cc6a6ae5862bacdb46f1c2
khanyusuf2001 <khan.yusuf2001@gmail.com>
2026-01-27 11:15:18 +0530

    First Push

---
+++ Blogger/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <h1>Blogger</h1>
+    </>
+  )
+}
+
+export default App
---
+++ Blogger/src/appwrite/auth.js
@@ -0,0 +1,63 @@
+import conf from "../conf/conf";
+
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService{
+    client = new Client();
+    account ;
+
+    constructor(){
+        this.client
+             .setEndpoint(conf.appwriteUrl)
+             .setProject(conf.appwriteProjectId);
+        this.account = new Account(this.client);
+    }
+
+    async createAccount({email, password, name}){
+        try{
+            const userAccount = await this.account.create(ID.unique(), email, password, name);
+            if (userAccount) {
+                // Call another method
+            } else {
+                return userAccount;
+            }
+
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async logIn({ email, password }) {
+        try {
+            return await this.account.createEmailPasswordSession(email, password)
+            
+        }catch(error){
+            throw error;
+        }
+    }
+
+    async getCurrentUser(){
+        try {
+            return await this.account.get();
+            
+        } catch (error) {
+            throw error;
+        }
+        return null;
+    }
+    async logOut(){
+        try{
+            this.account.deleteSessions();
+        }catch(error){
+            throw error;
+        }
+    }
+    
+    
+    }
+
+const authSerive = new AuthService();
+
+
+export default AuthService;
+
